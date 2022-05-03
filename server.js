dca52ec2abdeedc644bdbbb4045ead813c427af2
VMLujanJr <vmlujanjr@outlook.com>
2022-05-03 15:11:18 -0500

    added 404 error

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const req = require('express/lib/request');
 // Create route for front-end to request data from...
 const { animals } = require('./data/animals.json');
 
@@ -53,16 +54,36 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 };
 
+// ...create new functionality; takes id and array of animals, and returns a single animal object.
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+
+    return result;
+}
+
 // ...add route to listen to.
 app.get('/api/animals', (req, res) => { // type... localhost:3001/api/animals ...in the browser
    let results = animals;
    
    if (req.query) {
-       results = filterByQuery(req.query, results);
+       results = filterByQuery(req.query, results); // ...req.query checks a list of animals... while... req.params.id...
    }
 
    res.json(results);
     /* res.json(animals); */ // if you want to send lots of information... change 'send' to 'json' or vice versa for a short message on the console.log('Hey!');
+});
+
+// ...add new route; must come after previous route!
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals) // req.params.id checks for a single unique animal... insted of... a list of animals.
+
+    // if no record exists for the animal being searched for... return 404 error
+    if (result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
 });
 
 // listen for requests... on port '3001'... there are other port options available...
