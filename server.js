935351226af0a8f6c555dc4e0ff3483efacd8e64
VMLujanJr <vmlujanjr@outlook.com>
2022-05-04 18:03:31 -0500

    added validation for server integrity

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const req = require('express/lib/request');
 // Create route for front-end to request data from...
@@ -7,6 +9,12 @@ const PORT = process.env.PORT || 3001;
 
 // instantiate...
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
 
 // ...filter functionality
 function filterByQuery(query, animalsArray) {
@@ -61,6 +69,41 @@ function findById(id, animalsArray) {
     return result;
 }
 
+// ...create separate function to handle taking the data from req.body and adding it to our animals.json file.
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    const animal = body;
+    animalsArray.push(animal);
+
+    /* This won't actually add information to animals.json. Keep in mind that whenever we use require() to import data or functionality, it's only reading the data and creating a copy of it to use in server.js. So nothing we do with the imported data will ever affect the content of the file from which that data came. */
+    // ...but this should do it...
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals:animalsArray }, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+}
+
+// ...add validation to our data
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+
+    return true;
+}
+
 // ...add route to listen to.
 app.get('/api/animals', (req, res) => { // type... localhost:3001/api/animals ...in the browser
    let results = animals;
@@ -83,6 +126,25 @@ app.get('/api/animals/:id', (req, res) => {
     }
     else {
         res.send(404);
+    }
+});
+
+// ...allow users of app to populate the server with data by sending data from the client side of the app to the server
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    }
+    else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
     }
 });
 
